663b3e5c14e0ab3a8b327c85a3db77e60ed74490
TechnoBoy <Dloadjunior@gmail.com>
2021-06-12 23:39:20 +0530

    Components Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddStudentComponent } from './add-student/add-student.component';
+import { ListtudentComponent } from './listtudent/listtudent.component';
+import { UpdatetudentComponent } from './updatetudent/updatetudent.component';
+import { ListStudentComponent } from './list-student/list-student.component';
+import { UpdateStudentComponent } from './update-student/update-student.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterationComponent } from './registeration/registeration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddStudentComponent,
+    ListtudentComponent,
+    UpdatetudentComponent,
+    ListStudentComponent,
+    UpdateStudentComponent,
+    LoginComponent,
+    RegisterationComponent
   ],
   imports: [
     BrowserModule,
