30f6e4312daef5a2523107039d7ce03e2dfa3220
TechnoBoy <Dloadjunior@gmail.com>
2021-06-12 23:46:23 +0530

    Added Route Components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import {AddStudentComponent} from './add-student/add-student.component';
+import {ListStudentComponent} from './list-student/list-student.component';
+import {UpdateStudentComponent} from './update-student/update-student.component';
+import {LoginComponent} from './login/login.component';
+import {RegisterationComponent}  from './registeration/registeration.component';
+const routes: Routes = [
+  {
+    component : AddStudentComponent,
+    path : 'add'
+  },
+  {
+    component : ListStudentComponent,
+    path : 'list'
+  },
+  {
+    component : UpdateStudentComponent,
+    path : 'update'
+  },
+  {
+    component : LoginComponent,
+    path : 'login'
+  },
+  {
+    component : RegisterationComponent,
+    path : 'register'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
