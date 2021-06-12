f9a74ca1b20afa42f93d5914007e80cee890e509
TechnoBoy <Dloadjunior@gmail.com>
2021-06-12 23:56:49 +0530

    Updated Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddStudentComponent } from './add-student/add-student.component';
-import { ListtudentComponent } from './listtudent/listtudent.component';
-import { UpdatetudentComponent } from './updatetudent/updatetudent.component';
 import { ListStudentComponent } from './list-student/list-student.component';
 import { UpdateStudentComponent } from './update-student/update-student.component';
 import { LoginComponent } from './login/login.component';
@@ -15,8 +13,8 @@ import { RegisterationComponent } from './registeration/registeration.component'
   declarations: [
     AppComponent,
     AddStudentComponent,
-    ListtudentComponent,
-    UpdatetudentComponent,
+    ListStudentComponent,
+    UpdateStudentComponent,
     ListStudentComponent,
     UpdateStudentComponent,
     LoginComponent,
