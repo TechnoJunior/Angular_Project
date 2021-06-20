bff43886bb9aa62e9fe7d370d79afdd504db385e
TechnoBoy <Dloadjunior@gmail.com>
2021-06-20 13:43:04 +0530

    Configured Service class

--- src/app/student-service.service.ts
+++ src/app/student-service.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentServiceService {
 
-  constructor() { }
+  readonly api_url = "http://localhost:59629/api/student";
+
+  constructor(private http_request : HttpClient) { }
+
+  getStudents() : Observable<any[]>{
+    return this.http_request.get<any[]>(this.api_url);
+  }
 }
